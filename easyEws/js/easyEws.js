e54cc05612ed5ef5707cc5ab8deca6ebf707e5c1
davecra <decyahoo-github@yahoo.com>
2016-08-03 02:38:24 -0400

    Added two functions for message handling: sendPlainTextEmailWithAttachment getMailItemMimeContent

--- easyEws/js/easyEws.js
+++ easyEws/js/easyEws.js
@@ -1,12 +1,13 @@
 /*!
- * easyEWS JavaScript Library v1.0.3
+ * easyEWS JavaScript Library v1.0.4
  * http://davecra.com
- *
+ * https://raw.githubusercontent.com/davecra/easyEWS/master/easyEws/js/easyEws.js
+ * 
  * Copyright David E. Craig and other contributors
  * Released under the MIT license
  * https://tldrlegal.com/license/mit-license
  *
- * Date: 2016-07-13T09:09EST
+ * Date: 2016-08-03T02:35EST
  */
 var easyEws = (function () {
     "use strict";
@@ -96,6 +97,93 @@ var easyEws = (function () {
             // make the EWS call
             asyncEws(soap, function (xmlDoc) {
                 successCallback("succeeded");
+            }, function (errorDetails) {
+                if (errorCallback != null)
+                    errorCallback(errorDetails);
+            }, function (debug) {
+                if (debugCallback != null)
+                    debugCallback(debug);
+            });
+        };
+
+        easyEws.sendPlainTextEmailWithAttachment = function (subject, body, to, attachmentName, attachmentMime, successCallback, errorCallback) {
+            /// <summary>
+            /// PUBLIC: creates a new emails message with a single attachment and sends it
+            /// </summary>
+            /// <param name="subject" type="String">The subject for the message to be sent</param>
+            /// <param name="body" type="String">The body of the message to be sent</param>
+            /// <param name="to" type="String">The email address of the recipient</param>
+            /// <param name="attachmentName" type="String">Name of the attachment</param>
+            /// <param name="attachmentMime" type="String">MIME content in Base64 for the attachment</param>
+            /// <param name="successCallback" type="Function">Callback with 'success' if compelted successfully - function(string) { }</param>
+            /// <param name="errorCallback" type="Function">Error handler callback - function(Error) { }</param>
+            /// <param name="debugCallback" type="Function">Debug handler returns raw XML - function(String) { }</param>
+            var soap = '<m:CreateItem MessageDisposition="SendAndSaveCopy">' +
+                       '    <m:Items>' +
+                       '        <t:Message>' +
+                       '            <t:Subject>' + subject + '</t:Subject>' +
+                       '            <t:Body BodyType="Text">' + body + '</t:Body>' +
+                       '            <t:Attachments>' +
+                       '                <t:ItemAttachment>' +
+                       '                    <t:Name>' + attachmentName + '</t:Name>' +
+                       '                    <t:IsInline>false</t:IsInline>' +
+                       '                    <t:Message>' +
+                       '                        <t:MimeContent CharacterSet="UTF-8">' + attachmentMime + '</t:MimeContent>' +
+                       '                    </t:Message>' +
+                       '                </t:ItemAttachment>' +
+                       '            </t:Attachments>' +
+                       '            <t:ToRecipients><t:Mailbox><t:EmailAddress>' + to + '</t:EmailAddress></t:Mailbox></t:ToRecipients>' +
+                       '        </t:Message>' +
+                       '    </m:Items>' +
+                       '</m:CreateItem>';
+
+            soap = getSoapHeader(soap);
+
+            // make the EWS call 
+            asyncEws(soap, function (xmlDoc) {
+                // Get the required response, and if it's NoError then all has succeeded, so tell the user.
+                // Otherwise, tell them what the problem was. (E.G. Recipient email addresses might have been
+                // entered incorrectly --- try it and see for yourself what happens!!)
+                var result = xmlDoc.getElementsByTagName("ResponseCode")[0].textContent;
+                if (result == "NoError") {
+                    successCallback(result);
+                }
+                else {
+                    if (errorCallback != null)
+                        errorCallback(result);
+                }
+            }, function (errorDetails) {
+                if (errorCallback != null)
+                    errorCallback(errorDetails);
+            }, function (debug) {
+                if (debugCallback != null)
+                    debugCallback(debug);
+            });
+        };
+
+        easyEws.getMailItemMimeContent = function (mailItemId, successCallback, errorCallback, debugCallback) {
+            /// <summary>
+            /// PUBLIC: gets the mail item as raw MIME data
+            /// </summary>
+            /// <param name="mailItemId" type="type"></param>
+            /// <param name="successCallback" type="Function">Callback with email message as MIME Base64 string - function(string) { } </param>
+            /// <param name="errorCallback" type="Function">Error handler callback - function(Error) { }</param>
+            /// <param name="debugCallback" type="Function">Debug handler returns raw XML - function(String) { }</param>
+            var soap =
+                '<m:GetItem>' +
+                '    <m:ItemShape>' +
+                '        <t:BaseShape>IdOnly</t:BaseShape>' +
+                '        <t:IncludeMimeContent>true</t:IncludeMimeContent>' +
+                '    </m:ItemShape>' +
+                '    <m:ItemIds>' +
+                '        <t:ItemId Id="' + mailItemId + '"/>' +
+                '    </m:ItemIds>' +
+                '</m:GetItem>';
+            soap = getSoapHeader(soap);
+            // make the EWS call 
+            asyncEws(soap, function (xmlDoc) {
+                //var content = xmlDoc.getElementsByTagName("MimeContent")[0].textContent;
+                successCallback(xmlDoc);
             }, function (errorDetails) {
                 if (errorCallback != null)
                     errorCallback(errorDetails);
